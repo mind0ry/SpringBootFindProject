86cb084f542f9edbec7d9b6e72901e000837e2ac
KIM MINSEOK <mindory144@gmail.com>
2025-12-10 15:28:02 +0900

    수정

--- src/test/java/com/sist/web/SpringBootFindProjectApplicationTests.java
+++ src/test/java/com/sist/web/SpringBootFindProjectApplicationTests.java
@@ -3,7 +3,7 @@ package com.sist.web;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SpringBootFindProjectApplicationTests {
 
 	@Test
